e543b18afb40b2e557698e7256b46d59ebc3b2d8
Fırat Salgür <firatsalgur@gmail.com>
2020-12-06 13:38:26 +0200

    Make find-match? consume all parameters

--- day1-1.fs
+++ day1-1.fs
@@ -10,8 +10,8 @@ variable fd
 : load-file   open read close ;
 
 : 2020?       + 2020 = ;
-: find-match? begin 2dup @ 2020? if @ exit then dup @ 0<> while cell+ repeat drop false ;
-: find-pair   begin dup @ over find-match? ?dup if rot drop exit else drop cell+ then again ;
+: find-match? begin 2dup @ 2020? if @ nip exit then dup @ 0<> while cell+ repeat 2drop false ;
+: find-pair   begin dup @ over find-match? ?dup if swap @ exit else cell+ then again ;
 : answer      load-file find-pair * u. ;
 
 answer
