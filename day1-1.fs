5c026047e5d047b2834bd5f496cb86a67e80c681
Fırat Salgür <firatsalgur@gmail.com>
2020-12-06 13:16:05 +0200

    Improve readability with suggestions from Veltas

--- day1-1.fs
+++ day1-1.fs
@@ -3,23 +3,17 @@ variable fd
 : input-file  s" day1-input.txt" ;
 
 : fd>pad      pad 80 fd @ read-line throw drop ;
-: read-line?  fd>pad dup 0= if drop true else pad swap evaluate , false then ;
+: read-line?  fd>pad ?dup if pad swap evaluate , false else true then ;
 : read        here begin read-line? until 0 , ;
 : open        input-file r/o open-file throw fd ! ;
 : close       fd @ close-file throw ;
 : load-file   open read close ;
 
 : 2020?       + 2020 = ;
-: next-cell   1 cells + ;
-: find-match  begin 2dup @ 2020? if @ exit then dup @ 0<> while next-cell repeat drop false ;
-: find-pair   begin dup @ over find-match dup 0<> if rot drop exit then 2drop next-cell again ;
+: find-match  begin 2dup @ 2020? if @ exit then dup @ 0<> while cell+ repeat drop false ;
+: find-pair   begin dup @ over find-match ?dup if rot drop exit else drop cell+ then again ;
 : answer      load-file find-pair * u. ;
 
 answer
 bye
-
-
-
-
-
 
