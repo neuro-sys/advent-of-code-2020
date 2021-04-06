fcf4942eee8db2b639460540d4fab2aed9d93e15
Firat Salgur <firatsalgur@protonmail.com>
2021-04-06 12:35:11 +0300

    Sanitize formatting

--- day1-1.fs
+++ day1-1.fs
@@ -1,15 +1,32 @@
+variable fd
+
 : input-file  s" day1-input.txt" ;
 
-: fd>pad      pad 80 3 pick read-line throw drop ;
+: fd>pad      pad 80 fd @ read-line throw drop ;
 : read-line?  fd>pad ?dup if pad swap evaluate , false else true then ;
 : read        here begin read-line? until 0 , ;
-: open        input-file r/o open-file throw ;
-: close       close-file throw ;
-: load-file   open read swap close ;
+: open        input-file r/o open-file throw fd ! ;
+: close       fd @ close-file throw ;
+: load-file   open read close ;
 
 : 2020?       + 2020 = ;
-: find-match? begin 2dup @ 2020? if @ nip exit then dup @ 0<> while cell+ repeat 2drop false ;
-: find-pair   begin dup @ over find-match? ?dup if swap @ exit else cell+ then again ;
+
+: find-match?
+  begin
+    2dup @ 2020? if @ nip exit then
+    dup @ 0<>
+  while
+    cell+
+  repeat
+  2drop false
+;
+
+: find-pair
+  begin
+    dup @ over find-match? ?dup if swap @ exit else cell+ then
+  again
+;
+
 : answer      load-file find-pair * u. ;
 
 answer
--- day1-2.fs
+++ day1-2.fs
@@ -1,36 +1,38 @@
+variable fd
+
 : input-file  s" day1-input.txt" ;
 
-\ takes fd as input and reads into 80 char pad area, returns number of
-\ chars read ( addr -- u )
-: fd>pad      pad 80 3 pick read-line throw drop ;
-
-\ takes fd as input, writes to current DP, and preserves fd and returns boolean
-\ indicating eof ( addr -- addr t )
+: fd>pad      pad 80 fd @ read-line throw drop ;
 : read-line?  fd>pad ?dup if pad swap evaluate , false else true then ;
-
-\ takes fd as input, and returns dictionary data containing zero
-\ terminated cells ( addr -- addr addr)
 : read        here begin read-line? until 0 , ;
+: open        input-file r/o open-file throw fd ! ;
+: close       fd @ close-file throw ;
+: load-file   open read close ;
 
-( -- fd )
-: open        input-file r/o open-file throw ;
+: 2020?  ( num1 num2 num3 -- t )  + + 2020 - 0= ;
 
-( addr -- )
-: close       close-file throw ;
+: find-match? ( num1 num2 addr --- num3 t )
+  begin
+    dup @ 3 pick 3 pick 2020?
+    if @ true exit then
+    cell+ dup @ 0=
+  until
+  2drop drop false
+;
 
-( -- addr )
-: load-file   open read swap close ;
+: find-pair? ( num1 addr --- num2 num3 t )
+  begin
+    dup @ 2 pick 2 pick find-match? if true exit then
+    cell+ dup @ 0=
+  until
+  drop false
+;
 
-( num1 num2 num3 -- t )
-: 2020?       + + 2020 - 0= ;
-
-( num1 num2 addr --- num3 t ) 
-: find-match? begin dup @ 3 pick 3 pick 2020? if @ true exit then cell+ dup @ 0<> while repeat 2drop drop false ;
-
-( num1 addr --- num2 num3 t ) 
-: find-pair?  begin dup @ 2 pick 2 pick find-match? if true exit then cell+ dup @ 0<> while repeat drop false ;
-
-( addr --- num1 num2 num3 ) 
-: find-triple begin dup @ over find-pair? if 3 roll drop 3 roll drop exit then drop cell+ again ;
+: find-triple ( addr --- num1 num2 num3 )
+  begin
+    dup @ over find-pair? if 3 roll drop 3 roll drop exit then
+    drop cell+
+  again
+;
 
 load-file find-triple * * u. drop
