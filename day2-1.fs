410c6b90bb6aad4a3c03b1de3cef29f9d42291f3
Firat Salgur <firatsalgur@protonmail.com>
2021-04-14 11:20:17 +0300

    Improve day2-1

--- day2-1.fs
+++ day2-1.fs
@@ -8,28 +8,28 @@ variable letter
 : open-file       input-file r/o open-file throw fd ! ;
 : close-file      fd @ close-file throw ;
 : read-line?      pad 80 fd @ read-line throw drop dup nread ! ;
-: get-line        pad nread @ ;
+: line            pad nread @ ;
 : 1/search        search 0= throw 1 /string ;
 : skip-dash       s" -" 1/search ;
 : skip-space      s"  " 1/search ;
 : skip-:          s" :" 1/search ;
-: parse-min       get-line skip-dash drop 1- pad - pad swap 0 0 2swap >number 2drop drop ;
-: parse-max       get-line skip-dash over >r skip-space drop 1- r@ - r> swap
-                  0 0 2swap >number 2drop drop ;
-: parse-letter    get-line skip-space over >r skip-: drop 1- r@ - r> swap drop c@ ;
+: to-number       0 0 2swap >number 2drop drop ;
+: parse-min       line skip-dash drop 1- pad - pad swap to-number ;
+: parse-max       line skip-dash over >r skip-space drop 1- r@ - r> swap to-number ;
+: parse-letter    line skip-space over >r skip-: drop 1- r@ - r> swap drop c@ ;
+: in-range?       min @ >= swap max @ <= and ;
+: letter-eq?      over c@ letter c@ = ;
+: inc-count       postpone r> postpone 1+ postpone >r ; immediate
+: skip-letter     1 /string ;
 
 : check-valid?
-  get-line skip-space skip-space
+  line skip-space skip-space
   0 >r
-  begin
-    dup 0<>
+  begin ?dup 0<>
   while
-    over c@ letter c@ = if r> 1+ >r then
-    1 /string
-  repeat
-  2drop
-  r@ min @ >=
-  r> max @ <= and
+    letter-eq? if inc-count then skip-letter
+  repeat drop
+  r@ r> in-range?
 ;
 
 : solve
